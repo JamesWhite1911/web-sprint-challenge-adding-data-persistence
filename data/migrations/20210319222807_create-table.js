18e551ac2b730b9af413386e2c869c79576eba82
James <JamesWhite1911@protonmail.com>
2021-03-19 18:50:46 -0400

    migrations done

---
+++ data/migrations/20210319222807_create-table.js
@@ -0,0 +1,43 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments("project_id");
+            tbl.text('project_name', 128).notNullable();
+            tbl.text('project_description');
+            tbl.boolean('project_completed').defaultTo(0);
+        })
+        .createTable('resources', tbl => {
+            tbl.increments("resource_id");
+            tbl.text('resource_name', 128).unique().notNullable();
+            tbl.text('resource_description');
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments("task_id");
+            tbl.text('task_decription').notNullable();
+            tbl.text('task_notes');
+            tbl.boolean('task_completed').defaultTo(0);
+            tbl.integer('project_id')
+                .unsigned().notNullable()
+                .references('project_id').inTable('projects')
+                .onDelete('RESTRICT')
+        })
+        .createTable('project_resources', tbl => {
+            tbl.increments("project_resource_id");
+            tbl.integer('project_id')
+                .unsigned().notNullable()
+                .references('project_id').inTable('projects')
+                .onDelete('RESTRICT')
+            tbl.integer('resource_id')
+                .unsigned().notNullable()
+                .references('resource_id').inTable('resources')
+                .onDelete('RESTRICT')
+        })
+};
+
+exports.down = function (knex) { //drop in reverse order!
+    return knex.schema
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server')
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
+    console.log(`\n** API running on port: ${PORT} **\n`);
 })
